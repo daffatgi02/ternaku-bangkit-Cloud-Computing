ce688180df465a0fb97345492e8d057556a4535e
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-05-16 04:51:20 +0700

    add auth control edited server,js

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ app.use('/api', medicineRoutes);
 // Menjalankan server
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port http://localhost:${port}`);
 });
