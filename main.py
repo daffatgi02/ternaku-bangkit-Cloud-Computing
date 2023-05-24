c9623a19179ac2bdedfb6b6f4930fbc22ec1cffe
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-05-24 11:52:05 +0700

    add intex.html and fix some minor bugs at main.py

--- main.py
+++ main.py
@@ -1,110 +1,90 @@
 from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 import secrets
+from flask_cors import CORS
+import jwt
+from flask_bcrypt import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'python'
-app.config['SECRET_KEY'] = secrets.token_hex(16)  # Generate a random SECRET_KEY
-
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['SECRET_KEY'] = secrets.token_hex(16)
 mysql = MySQL(app)
+CORS(app)
 
 
-@app.route('/api/auth/login', methods=['POST'])
-def login():
-    email = request.form['email']
-    password = request.form['password']
+@app.route('/api/auth/register', methods=['POST'])
+def register():
+    email = request.json['email']
+    password = request.json['password']
+    fullname = request.json['fullname']  # Menambahkan pengambilan fullname
 
+    # Check if email already exists
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM users WHERE email = %s", (email,))
     user = cur.fetchone()
     cur.close()
 
     if user:
-        # Check password
-        if user['password_hash'] == password:
-            login_result = {
-                'email': user['email'],
-                'fullname': user['fullname'],
-                'token': secrets.token_hex(16),
-                'userid': user['id']
-            }
-            return jsonify({
-                'error': False,
-                'loginResult': login_result,
-                'message': 'Login Success'
-            })
-        else:
-            return jsonify({
-                'error': True,
-                'message': 'Wrong Password'
-            })
-    else:
-        return jsonify({
-            'error': True,
-            'message': 'Account not found'
-        })
+        return jsonify({'error': True, 'message': 'Email already taken'})
+
+    # Create a new user
+    cur = mysql.connection.cursor()
+    hashed_password = generate_password_hash(password).decode('utf-8')
+    cur.execute("INSERT INTO users (email, password, fullname) VALUES (%s, %s, %s)", (email, hashed_password, fullname))  # Menyimpan fullname dalam database
+    mysql.connection.commit()
+    cur.close()
+
+    return jsonify({'error': False, 'message': 'User Created'})
 
 
-@app.route('/api/auth/register', methods=['POST'])
-def register():
-    email = request.form['email']
+
+@app.route('/api/auth/getdetail', methods=['GET'])
+def get_user_detail():
+    token = request.headers.get('Authorization').split()[1]
+    if not token:
+        return jsonify({'error': True, 'message': 'Missing token'})
+    try:
+        decoded_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        user_id = decoded_token['userid']
+    except jwt.ExpiredSignatureError:
+        return jsonify({'error': True, 'message': 'Invalid token'})
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT email, fullname, user_id FROM users WHERE user_id = %s", (user_id,))
+    user = cur.fetchone()
+    cur.close()
+
+    if user:
+        return jsonify(user)
+
+    return jsonify({'error': True, 'message': 'User Not Found'})
+
+
+@app.route('/api/auth/login', methods=['POST'])
+def login():
+    email = request.json['email']
+    password = request.json['password']
 
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM users WHERE email = %s", (email,))
     user = cur.fetchone()
+    cur.close()
 
-    if user:
-        return jsonify({
-            'error': True,
-            'message': 'Email already taken'
-        })
-    else:
-        password_salt = request.form['password_salt']
-        password_hash = request.form['password_hash']
+    if user and check_password_hash(user['password'], password):
+        token = jwt.encode({'userid': user['user_id']}, app.config['SECRET_KEY'], algorithm='HS256')
+        login_result = {
+            'email': user['email'],
+            'fullname': user['fullname'],
+            'token': token,
+            'userid': user['user_id']
+        }
+        return jsonify({'error': False, 'loginResult': login_result, 'message': 'Login Success'})
 
-        cur.execute("INSERT INTO users (email, password_salt, password_hash) VALUES (%s, %s, %s)",
-                    (email, password_salt, password_hash))
-        mysql.connection.commit()
-        cur.close()
-
-        return jsonify({
-            'error': False,
-            'message': 'User Created'
-        })
-
-@app.route('/api/auth/getdetail', methods=['GET'])
-def get_detail():
-    token = request.headers.get('Authorization').split()[1]
-
-    try:
-        decoded_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-        user_id = decoded_token['user_id']
-
-        cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM users WHERE id = %s", (user_id,))
-        user = cur.fetchone()
-        cur.close()
-
-        if user:
-            user_detail = {
-                'email': user['email'],
-                'fullname': user['fullname'],
-                'user_id': user['id']
-            }
-            return jsonify(user_detail)
-        else:
-            return jsonify({
-                'error': True,
-                'message': 'User not found'
-            })
-    except jwt.DecodeError:
-        return jsonify({
-            'error': True,
-            'message': 'Invalid token'
-        })
+    return jsonify({'error': True, 'message': 'Wrong Password or Account not found'})
 
 
 if __name__ == '__main__':
