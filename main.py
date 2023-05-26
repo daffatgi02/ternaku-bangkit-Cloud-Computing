ff813bfc5fb7607cf924bb7a6887aad0d0dddd8d
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-05-26 08:07:42 +0700

    give auth token

--- main.py
+++ main.py
@@ -18,6 +18,47 @@ mysql = MySQL(app)
 CORS(app)
 
 
+def validate_token(token):
+    if not token:
+        return False, 'Missing token'
+    try:
+        decoded_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        user_id = decoded_token['userid']
+        return True, user_id
+    except jwt.ExpiredSignatureError:
+        return False, 'Token Invalid. Please login or register!'
+    except jwt.DecodeError:
+        return False, 'Token Invalid. Please login or register!'
+
+
+@app.route('/api/products', methods=['GET'])
+def get_products():
+    token = request.headers.get('Authorization', '').split()[1]
+    valid_token, user_id = validate_token(token)
+    if not valid_token:
+        return jsonify({'error': True, 'message': user_id}), 500
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM products")
+    products = cur.fetchall()
+    cur.close()
+    return jsonify({'products': products})
+
+
+@app.route('/api/articles', methods=['GET'])
+def get_articles():
+    token = request.headers.get('Authorization', '').split()[1]
+    valid_token, user_id = validate_token(token)
+    if not valid_token:
+        return jsonify({'error': True, 'message': user_id}), 500
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM articles")
+    articles = cur.fetchall()
+    cur.close()
+    return jsonify({'articles': articles})
+
+
 @app.route('/api/auth/register', methods=['POST'])
 def register():
     email = request.json['email']
@@ -25,7 +66,7 @@ def register():
     fullname = request.json['fullname']
 
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM users WHERE email = %s", (email, ))
+    cur.execute("SELECT * FROM users WHERE email = %s", (email,))
     user = cur.fetchone()
     cur.close()
 
@@ -49,21 +90,19 @@ def login():
     password = request.json['password']
 
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM users WHERE email = %s", (email, ))
+    cur.execute("SELECT * FROM users WHERE email = %s", (email,))
     user = cur.fetchone()
     cur.close()
 
     if user and check_password_hash(user['password'], password):
-        exp_time = datetime.utcnow() + timedelta(
-            days=2)  # Set expiration time 2 days from now
-        exp_time = int(time.mktime(
-            exp_time.timetuple()))  # Convert to UNIX timestamp
+        exp_time = datetime.utcnow() + timedelta(days=2)  # Set expiration time 2 days from now
+        exp_time = int(time.mktime(exp_time.timetuple()))  # Convert to UNIX timestamp
         token = jwt.encode({
             'userid': user['user_id'],
             'exp': exp_time
         },
-                           app.config['SECRET_KEY'],
-                           algorithm='HS256')
+            app.config['SECRET_KEY'],
+            algorithm='HS256')
         login_result = {
             'email': user['email'],
             'fullname': user['fullname'],
@@ -84,29 +123,15 @@ def login():
 
 @app.route('/api/auth/getdetail', methods=['GET'])
 def get_user_detail():
-    token = request.headers.get('Authorization').split()[1]
-    if not token:
-        return jsonify({'error': True, 'message': 'Missing token'}), 500
-    try:
-        decoded_token = jwt.decode(token,
-                                   app.config['SECRET_KEY'],
-                                   algorithms=['HS256'])
-        user_id = decoded_token['userid']
-    except jwt.ExpiredSignatureError:
-        return jsonify({
-            'error': True,
-            'message': 'Token Invalid. Please login or register !'
-        }), 500
-    except jwt.DecodeError:
-        return jsonify({
-            'error': True,
-            'message': 'Token Invalid. Please login or register !'
-        }), 500
+    token = request.headers.get('Authorization', '').split()[1]
+    valid_token, user_id = validate_token(token)
+    if not valid_token:
+        return jsonify({'error': True, 'message': user_id}), 500
 
     cur = mysql.connection.cursor()
     cur.execute(
         "SELECT email, fullname, user_id FROM users WHERE user_id = %s",
-        (user_id, ))
+        (user_id,))
     user = cur.fetchone()
     cur.close()
 
@@ -118,27 +143,13 @@ def get_user_detail():
 
 @app.route('/api/homepage', methods=['GET'])
 def get_homepage():
-    token = request.headers.get('Authorization').split()[1]
-    if not token:
-        return jsonify({'error': True, 'message': 'Missing token'}), 500
-    try:
-        decoded_token = jwt.decode(token,
-                                   app.config['SECRET_KEY'],
-                                   algorithms=['HS256'])
-        user_id = decoded_token['userid']
-    except jwt.ExpiredSignatureError:
-        return jsonify({
-            'error': True,
-            'message': 'Token Invalid. Please login or register !'
-        }), 500
-    except jwt.DecodeError:
-        return jsonify({
-            'error': True,
-            'message': 'Token Invalid. Please login or register !'
-        }), 500
+    token = request.headers.get('Authorization', '').split()[1]
+    valid_token, user_id = validate_token(token)
+    if not valid_token:
+        return jsonify({'error': True, 'message': user_id}), 500
 
     cur = mysql.connection.cursor()
-    cur.execute("SELECT fullname FROM users WHERE user_id = %s", (user_id, ))
+    cur.execute("SELECT fullname FROM users WHERE user_id = %s", (user_id,))
     user = cur.fetchone()
     cur.close()
 
@@ -151,19 +162,15 @@ def get_homepage():
     return jsonify({'error': True, 'message': 'User Not Found'}), 500
 
 
-@app.route('/api/products', methods=['GET'])
-def get_products():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM products")
-    products = cur.fetchall()
-    cur.close()
-    return jsonify({'products': products})
-
-
 @app.route('/api/products/<int:product_id>', methods=['GET'])
 def get_product(product_id):
+    token = request.headers.get('Authorization', '').split()[1]
+    valid_token, user_id = validate_token(token)
+    if not valid_token:
+        return jsonify({'error': True, 'message': user_id}), 500
+
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM products WHERE id = %s", (product_id, ))
+    cur.execute("SELECT * FROM products WHERE id = %s", (product_id,))
     product = cur.fetchone()
     cur.close()
 
@@ -173,20 +180,15 @@ def get_product(product_id):
     return jsonify({'error': True, 'message': 'Product Not Found'}), 404
 
 
-@app.route('/api/articles', methods=['GET'])
-
-def get_articles():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM articles")
-    articles = cur.fetchall()
-    cur.close()
-    return jsonify({'articles': articles})
-
-
 @app.route('/api/articles/<int:article_id>', methods=['GET'])
 def get_article(article_id):
+    token = request.headers.get('Authorization', '').split()[1]
+    valid_token, user_id = validate_token(token)
+    if not valid_token:
+        return jsonify({'error': True, 'message': user_id}), 500
+
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM articles WHERE id = %s", (article_id, ))
+    cur.execute("SELECT * FROM articles WHERE id = %s", (article_id,))
     article = cur.fetchone()
     cur.close()
 
