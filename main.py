347215f6451099b6621966140be209db33f948a1
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-06-03 12:12:00 +0700

    change libary, add new endpoint

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, jsonify
-from flask_mysqldb import MySQL
+import jwt
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, decode_token
+from flask_jwt_extended.exceptions import JWTDecodeError
+from jwt import ExpiredSignatureError
 import secrets
 from flask_cors import CORS
-import jwt
 from flask_bcrypt import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
 import time
+import pymysql.cursors
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import numpy as np
@@ -17,14 +20,20 @@ import uuid
 
 app = Flask(__name__)
 CORS(app)
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '34.143.132.191'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = 'daffa123'
 app.config['MYSQL_DB'] = 'python'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-app.config['SECRET_KEY'] = secrets.token_hex(16)
-mysql = MySQL(app)
-
+app.config['MYSQL_CURSORCLASS'] = 'pymysql.cursors.DictCursor'
+app.config['JWT_SECRET_KEY'] = secrets.token_hex(16)
+db = pymysql.connections.Connection(
+    host=app.config['MYSQL_HOST'],
+    user=app.config['MYSQL_USER'],
+    password=app.config['MYSQL_PASSWORD'],
+    db=app.config['MYSQL_DB'],
+    cursorclass=pymysql.cursors.DictCursor
+)
+jwt = JWTManager(app)
 # key json google serviceacc
 service_account_key_path = 'key.json'
 
@@ -87,42 +96,15 @@ def upload_image_to_gcs(image):
 def validate_token(token):
     if not token:
         return False, 'Missing token'
+
     try:
-        decoded_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-        user_id = decoded_token['userid']
+        decoded_token = decode_token(token)
+        user_id = decoded_token['sub']
         return True, user_id
-    except jwt.ExpiredSignatureError:
-        return False, 'Token Invalid. Please login or register!'
-    except jwt.DecodeError:
-        return False, 'Token Invalid. Please login or register!'
-
-
-@app.route('/api/products', methods=['GET'])
-def get_products():
-    token = request.headers.get('Authorization', '').split()[1]
-    valid_token, user_id = validate_token(token)
-    if not valid_token:
-        return jsonify({'error': True, 'message': user_id}), 500
-
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM products")
-    products = cur.fetchall()
-    cur.close()
-    return jsonify({'products': products})
-
-
-@app.route('/api/articles', methods=['GET'])
-def get_articles():
-    token = request.headers.get('Authorization', '').split()[1]
-    valid_token, user_id = validate_token(token)
-    if not valid_token:
-        return jsonify({'error': True, 'message': user_id}), 500
-
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM articles")
-    articles = cur.fetchall()
-    cur.close()
-    return jsonify({'articles': articles})
+    except ExpiredSignatureError:
+        return False, 'Token has expired. Please log in again.'
+    except (JWTDecodeError, KeyError):
+        return False, 'Invalid token. Please log in again.'
 
 
 @app.route('/api/auth/register', methods=['POST'])
@@ -131,7 +113,7 @@ def register():
     password = request.json['password']
     fullname = request.json['fullname']
 
-    cur = mysql.connection.cursor()
+    cur = db.cursor()
     cur.execute("SELECT * FROM users WHERE email = %s", (email,))
     user = cur.fetchone()
     cur.close()
@@ -139,23 +121,22 @@ def register():
     if user:
         return jsonify({'error': True, 'message': 'Email already taken'})
 
-    cur = mysql.connection.cursor()
+    cur = db.cursor()
     hashed_password = generate_password_hash(password).decode('utf-8')
     cur.execute(
         "INSERT INTO users (email, password, fullname) VALUES (%s, %s, %s)",
         (email, hashed_password, fullname))
-    mysql.connection.commit()
+    db.commit()
     cur.close()
 
     return jsonify({'error': False, 'message': 'User Created'})
-
 
 @app.route('/api/auth/login', methods=['POST'])
 def login():
     email = request.json['email']
     password = request.json['password']
 
-    cur = mysql.connection.cursor()
+    cur = db.cursor()
     cur.execute("SELECT * FROM users WHERE email = %s", (email,))
     user = cur.fetchone()
     cur.close()
@@ -163,12 +144,7 @@ def login():
     if user and check_password_hash(user['password'], password):
         exp_time = datetime.utcnow() + timedelta(days=2)  # Set expiration time 2 days from now
         exp_time = int(time.mktime(exp_time.timetuple()))  # Convert to UNIX timestamp
-        token = jwt.encode({
-            'userid': user['user_id'],
-            'exp': exp_time
-        },
-            app.config['SECRET_KEY'],
-            algorithm='HS256')
+        token = create_access_token(identity=user['user_id'], expires_delta=timedelta(days=2))
         login_result = {
             'email': user['email'],
             'fullname': user['fullname'],
@@ -186,85 +162,8 @@ def login():
         'message': 'Wrong Password or Account not found'
     })
 
-
-@app.route('/api/auth/getdetail', methods=['GET'])
-def get_user_detail():
-    token = request.headers.get('Authorization', '').split()[1]
-    valid_token, user_id = validate_token(token)
-    if not valid_token:
-        return jsonify({'error': True, 'message': user_id}), 500
-
-    cur = mysql.connection.cursor()
-    cur.execute(
-        "SELECT email, fullname, user_id FROM users WHERE user_id = %s",
-        (user_id,))
-    user = cur.fetchone()
-    cur.close()
-
-    if user:
-        return jsonify(user)
-
-    return jsonify({'error': True, 'message': 'User Not Found'}), 500
-
-
-@app.route('/api/homepage', methods=['GET'])
-def get_homepage():
-    token = request.headers.get('Authorization', '').split()[1]
-    valid_token, user_id = validate_token(token)
-    if not valid_token:
-        return jsonify({'error': True, 'message': user_id}), 500
-
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT fullname FROM users WHERE user_id = %s", (user_id,))
-    user = cur.fetchone()
-    cur.close()
-
-    if user:
-        return jsonify({
-            'message':
-            f"Hello! Welcome to the homepage, {user['fullname']}."
-        })
-
-    return jsonify({'error': True, 'message': 'User Not Found'}), 500
-
-
-@app.route('/api/products/<int:product_id>', methods=['GET'])
-def get_product(product_id):
-    token = request.headers.get('Authorization', '').split()[1]
-    valid_token, user_id = validate_token(token)
-    if not valid_token:
-        return jsonify({'error': True, 'message': user_id}), 500
-
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM products WHERE id = %s", (product_id,))
-    product = cur.fetchone()
-    cur.close()
-
-    if product:
-        return jsonify({'product': product})
-
-    return jsonify({'error': True, 'message': 'Product Not Found'}), 404
-
-
-@app.route('/api/articles/<int:article_id>', methods=['GET'])
-def get_article(article_id):
-    token = request.headers.get('Authorization', '').split()[1]
-    valid_token, user_id = validate_token(token)
-    if not valid_token:
-        return jsonify({'error': True, 'message': user_id}), 500
-
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM articles WHERE id = %s", (article_id,))
-    article = cur.fetchone()
-    cur.close()
-
-    if article:
-        return jsonify({'article': article})
-
-    return jsonify({'error': True, 'message': 'Article Not Found'}), 404
-
 # Endpoint /predictsapi
-@app.route('/predictsapi', methods=['POST'])
+@app.route('/api/predictsapi', methods=['POST'])
 def predict_sapi():
     if 'image' not in request.files:
         return jsonify({'error': 'No image uploaded'})
@@ -282,7 +181,7 @@ def predict_sapi():
     return jsonify(result)
 
 # Endpoint /predictkambing
-@app.route('/predictkambing', methods=['POST'])
+@app.route('/api/predictkambing', methods=['POST'])
 def predict_kambing():
     if 'image' not in request.files:
         return jsonify({'error': 'No image uploaded'})
@@ -299,10 +198,105 @@ def predict_kambing():
     result = {'class': predicted_class, 'probability': float(probability), 'image_url': image_url}
     return jsonify(result)
 
+# Endpoint /api/products
+@app.route('/api/products', methods=['GET'])
+@jwt_required()
+def get_products():
+    cur = db.cursor()
+    cur.execute("SELECT * FROM products")
+    products = cur.fetchall()
+    cur.close()
+
+    return jsonify(products)
+
+# Endpoint /api/products/<int:product_id>
+@app.route('/api/products/<int:product_id>', methods=['GET'])
+@jwt_required()
+def get_product(product_id):
+    cur = db.cursor()
+    cur.execute("SELECT * FROM products WHERE id = %s", (product_id,))
+    product = cur.fetchone()
+    cur.close()
+
+    if not product:
+        return jsonify({'error': True, 'message': 'Product not found'})
+
+    return jsonify({'error': False, 'product': product})
+
+# Endpoint /api/articles
+@app.route('/api/articles', methods=['GET'])
+def get_articles():
+    cur = db.cursor()
+    cur.execute("SELECT * FROM articles")
+    articles = cur.fetchall()
+    cur.close()
+
+    return jsonify({'articles': articles})
+
+# Endpoint /api/articles/<int:article_id>
+@app.route('/api/articles/<int:article_id>', methods=['GET'])
+def get_article(article_id):
+    cur = db.cursor()
+    cur.execute("SELECT * FROM articles WHERE id = %s", (article_id,))
+    article = cur.fetchone()
+    cur.close()
+
+    if article:
+        return jsonify({'article': article})
+    else:
+        return jsonify({'error': 'Article not found'})
+
+@app.route('/api/profile', methods=['GET'])
+@jwt_required()
+def get_profile():
+    token = request.headers.get('Authorization').split()[1]
+    is_valid, user_id = validate_token(token)
+
+    if not is_valid:
+        return jsonify({'error': True, 'message': user_id})
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM users WHERE user_id = %s", (user_id,))
+    user = cur.fetchone()
+    cur.close()
+
+    if not user:
+        return jsonify({'error': True, 'message': 'User not found'})
+
+    profile = {
+        'email': user['email'],
+        'fullname': user['fullname']
+    }
+
+    return jsonify({'error': False, 'profile': profile})
+    
+# Endpoint homepage
+@app.route('/homepage')
+@jwt_required()
+def homepage():
+    token = request.headers.get('Authorization').split()[1]
+    is_valid, user_id = validate_token(token)
+
+    if not is_valid:
+        return jsonify({'error': True, 'message': user_id})
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM users WHERE user_id = %s", (user_id,))
+    user = cur.fetchone()
+    cur.close()
+
+    if not user:
+        return jsonify({'error': True, 'message': 'User not found'})
+
+    fullname = user['fullname']
+
+    return f'Selamat Datang, {fullname}'
+
+
 # Endpoint root
 @app.route('/')
 def index():
     return 'Welcome to the Ternaku'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
